906f7183566ef5bb3b5ebf529883c987926f62ac
ChumaDaCabbage <1707224@mukilteo.wednet.edu>
2022-01-30 00:12:34 -0800

    Title is now centered better

--- CRPG/GameEngine.cs
+++ CRPG/GameEngine.cs
@@ -12,28 +12,33 @@ namespace CRPG
         public static void Initialize()
         {
             //Set screen size
-            Console.SetWindowSize(Console.LargestWindowWidth / 2, Console.LargestWindowHeight - (Console.LargestWindowHeight / 3));
+            Console.SetWindowSize(120, 42);
 
             //Start message
-            Console.WriteLine("                                       Initializing Game Engine Version {0}", Version);
-            Console.WriteLine();
-            Console.WriteLine();
+            Console.SetCursorPosition(41, 0);
+            Console.WriteLine("Initializing Game Engine Version {0}", Version);
+
 
             //Credits to https://patorjk.com/software/taag/ for the awesome ascii art
+            Console.SetCursorPosition(22, 3);
+            Console.WriteLine("   ▄████████    ▄████████  ▄████████    ▄████████    ▄███████▄    ▄████████ ");
+            Console.SetCursorPosition(22, 4);
+            Console.WriteLine("  ███    ███   ███    ███ ███    ███   ███    ███   ███    ███   ███    ███ ");
+            Console.SetCursorPosition(22, 5);
+            Console.WriteLine("  ███    █▀    ███    █▀  ███    █▀    ███    ███   ███    ███   ███    █▀  ");
+            Console.SetCursorPosition(22, 6);
+            Console.WriteLine(" ▄███▄▄▄       ███        ███          ███    ███   ███    ███  ▄███▄▄▄     ");
+            Console.SetCursorPosition(22, 7);
+            Console.WriteLine("▀▀███▀▀▀     ▀███████████ ███        ▀███████████ ▀█████████▀  ▀▀███▀▀▀     ");
+            Console.SetCursorPosition(22, 8);
+            Console.WriteLine("  ███    █▄           ███ ███    █▄    ███    ███   ███          ███    █▄  ");
+            Console.SetCursorPosition(22, 9);
+            Console.WriteLine("  ███    ███    ▄█    ███ ███    ███   ███    ███   ███          ███    ███ ");
+            Console.SetCursorPosition(22, 10);
+            Console.WriteLine("  ██████████  ▄████████▀  ████████▀    ███    █▀   ▄████▀        ██████████ ");
 
-            Console.WriteLine("                       ▄████████    ▄████████  ▄████████    ▄████████    ▄███████▄    ▄████████ ");
-            Console.WriteLine("                      ███    ███   ███    ███ ███    ███   ███    ███   ███    ███   ███    ███ ");
-            Console.WriteLine("                      ███    █▀    ███    █▀  ███    █▀    ███    ███   ███    ███   ███    █▀  ");
-            Console.WriteLine("                     ▄███▄▄▄       ███        ███          ███    ███   ███    ███  ▄███▄▄▄     ");
-            Console.WriteLine("                    ▀▀███▀▀▀     ▀███████████ ███        ▀███████████ ▀█████████▀  ▀▀███▀▀▀     ");
-            Console.WriteLine("                      ███    █▄           ███ ███    █▄    ███    ███   ███          ███    █▄  ");
-            Console.WriteLine("                      ███    ███    ▄█    ███ ███    ███   ███    ███   ███          ███    ███ ");
-            Console.WriteLine("                      ██████████  ▄████████▀  ████████▀    ███    █▀   ▄████▀        ██████████ ");
-
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine("                                              Press any Key to begin!");
+            Console.SetCursorPosition(48, 13);
+            Console.WriteLine("Press any Key to begin!");
             Console.ReadKey();
             Console.Clear();
         }
